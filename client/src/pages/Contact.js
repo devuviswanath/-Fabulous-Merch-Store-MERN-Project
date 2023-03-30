b7254a80ded45b72f61877af0ec985a2eafa0adc
devuv <devuviswanath1996@gmail.com>
2023-03-30 11:42:59 -0400

    contact page

---
+++ client/src/pages/Contact.js
@@ -0,0 +1,101 @@
+import React from "react";
+import { AiOutlineHome, AiOutlineMail } from "react-icons/ai";
+import { BiPhoneCall, BiInfoCircle } from "react-icons/bi";
+
+const Contact = () => {
+  return (
+    <>
+      {/* <meta title={"Contact Us"}/> */}
+      {/* breadCrumb */}
+      <div className="contact-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2887.9970721810705!2d-79.67672018465905!3d43.627419879122144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b409fb8a947f9%3A0x418640e93fdafd13!2sLambton%20College!5e0!3m2!1sen!2sca!4v1680145927622!5m2!1sen!2sca"
+                width="600"
+                height="450"
+                className="border-0 w-100"
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+            <div className="=col-12 mt-5">
+              <div className="contact-inner-wrapper d-flex justify-content-between">
+                <div>
+                  <h3 className="contact-title">Contact</h3>
+                  <form action="" className="d-flex flex-column gap-15">
+                    <div>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Name"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Email"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="tel"
+                        className="form-control"
+                        placeholder="Mobile Number"
+                      />
+                    </div>
+                    <div>
+                      <textarea
+                        name=""
+                        className="w-100 form-control"
+                        id=""
+                        cols="30"
+                        rows="4"
+                        placeholder="Comments"
+                      ></textarea>
+                    </div>
+                    <div>
+                      <button className="button border-0">Submit</button>
+                    </div>
+                  </form>
+                </div>
+                <div>
+                  <h3 className="contact-title mb-4">Get in touch with us</h3>
+                  <div>
+                    <ul className="ps-0">
+                      <li className="mb-3 d-flex gap-15 align-item-center">
+                        <AiOutlineHome className="fs-5" />
+                        <address className="mb-0">
+                          Lambton College, 121 Brunel Rd, Mississauga
+                        </address>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-item-center">
+                        <BiPhoneCall className="fs-5" />
+                        <a href="tel:905-890-7833">905-890-7833</a>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-item-center">
+                        <AiOutlineMail className="fs-5" />
+                        <a href="mailto:lambton@​queenscollege.ca">
+                          lambton@​queenscollege.ca
+                        </a>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-item-center">
+                        <BiInfoCircle className="fs-5" />
+                        <p className="mb-0">Monday - Friday 10 AM to 8 PM</p>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
