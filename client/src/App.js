d7c92d5c4f9003fa16ea1c85d855d65115c97c76
devuv <devuviswanath1996@gmail.com>
2023-03-30 13:50:48 -0400

    conflict finla

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,11 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-<<<<<<< Updated upstream
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
-=======
-import Contact from "./pages/Contact";
->>>>>>> Stashed changes
 
 function App() {
   return (
@@ -18,15 +14,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-<<<<<<< Updated upstream
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
             <Route path="cart" element={<Cart />} />
             <Route path="login" element={<Login />} />
-=======
-            <Route path="contact" element={<Contact />} />
->>>>>>> Stashed changes
           </Route>
         </Routes>
       </BrowserRouter>
