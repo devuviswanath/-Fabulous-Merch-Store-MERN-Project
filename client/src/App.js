8cf756b75eeb235dbd7e1734398f9ae25b529aee
jainj <jainj@DESKTOP-0JSU9DK>
2023-04-09 19:50:50 -0400

    product pages added

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import OurStore from "./pages/OurStore";
+import SingleProduct from "./pages/SingleProduct";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import Forgotpassword from "./pages/Forgotpassword";
@@ -26,6 +28,8 @@ function App() {
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
+            <Route path="product" element={<OurStore />} />
+            <Route path="product/:id" element={<SingleProduct />} />
             <Route path="cart" element={<Cart />} />
             <Route path="login" element={<Login />} />
             <Route path="forgot-password" element={<Forgotpassword />} />
